b0f1557c1c0b7afe7a3b7b05f370cdf2b95a5106
bart <bgroffen@avans.nl>
2014-12-15 12:41:51 +0100

    DEZE SHIT IN DE MODELL

---
+++ Breda Maps/Breda Maps/Model/Route.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+
+namespace Breda_Maps.Model
+{
+    class Route
+    {
+        private List<Geopoint> points = new List<Geopoint>();
+
+        private Route()
+        {
+            addpoint();
+        }
+
+        private void addpoint()
+        {
+
+            Geopoint testpoint = new Geopoint(new BasicGeoposition() { Latitude = 51.59380, Longitude = 4.77963 }); // point van VVV Breda
+            Geopoint testpoint2 = new Geopoint(new BasicGeoposition() { Latitude = 51.59307 , Longitude =4.77969 }); // point van Liefdeszuster
+
+            points.Add(testpoint);
+            points.Add(testpoint2);
+        }
+
+    }
+}
---
+++ Breda Maps/Breda Maps/Model/Sight.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace Breda_Maps.Model
+{
+    class Sight
+    {
+        private String description;
+        private Geoposition location;
+
+        private Sight(String description, Geoposition location)
+        {
+            this.description = description;
+            this.location = location;
+        }
+
+        private Geoposition getLocation()
+        {
+
+            return location;
+        }
+
+        private String getDescription()
+        {
+            return this.description;
+        }
+    }
+}
