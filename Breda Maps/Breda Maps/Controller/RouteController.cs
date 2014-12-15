450a542ba8b424676dfac9abf6e289ce76dc682a
bart <bgroffen@avans.nl>
2014-12-15 13:30:51 +0100

    fixed controll package

--- Breda Maps/Breda Maps/Controller/RouteController.cs
+++ Breda Maps/Breda Maps/Controller/RouteController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,19 @@ namespace Breda_Maps.Controller
         private List<Route> routes = new List<Route>(); 
         private List<Sight> sights = new List<Sight>();
 
+        private RouteController()
+        {
+            
+        }
+        private Void addRoute(Route route)
+        {
+            routes.Add(route);
+        }
 
+        private Route selectRoute(Route route)
+        {
+            //selecteer een route ?
+            return route;
+        }
     }
 }
