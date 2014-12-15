5cfebcded6ff6e60dc7f367c609a5151df270284
bart <bgroffen@avans.nl>
2014-12-15 13:07:54 +0100

    started controll class

---
+++ Breda Maps/Breda Maps/Controller/RouteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Breda_Maps.Model;
+
+namespace Breda_Maps.Controller
+{
+    class RouteController
+    {
+        private List<Route> routes = new List<Route>(); 
+        private List<Sight> sights = new List<Sight>();
+
+
+    }
+}
