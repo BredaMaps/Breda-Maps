d3db22b3e9a4cc317948f99738c91a2ca4e24395
rudyjason <rudyjason.ep@hotmail.com>
2015-01-05 10:52:06 +0100

    testroutes

--- Breda Maps/Breda Maps/Controller/RouteController.cs
+++ Breda Maps/Breda Maps/Controller/RouteController.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Breda_Maps.Model;
 using Breda_Maps.View;
+using Windows.Devices.Geolocation;
 
 namespace Breda_Maps.Controller
 {
@@ -20,11 +21,6 @@ namespace Breda_Maps.Controller
         public RouteController()
         {
             init();
-           
-            routes.Add(new Route("Test 1"));
-            routes.Add(new Route("Test 2"));
-            routes.Add(new Route("Test 3"));
-            routes.Add(new Route("Test 4"));
         }
 
         public RouteController(MainPage mp)
@@ -101,12 +97,28 @@ namespace Breda_Maps.Controller
 #region TEST
         private void TestFillRoutes()
         {
-            // create and fill routes
+            Route testRoute = new Route("Real Test");
+            foreach(Sight s in sights)
+            {
+                testRoute.addpoint(s);
+            }
+            routes.Add(testRoute);
+            routes.Add(new Route("Test 1"));
+            routes.Add(new Route("Test 2"));
+            routes.Add(new Route("Test 3"));
+            routes.Add(new Route("Test 4"));
         }
 
         private void TestFillSights()
         {
-            // create and fill sights
+            sights.Add(new Sight("VVV Breda", new Geopoint(new BasicGeoposition() { Latitude = 51.59380, Longitude = 4.77963 }), Enums.EnumCat.FACILITY));
+            sights.Add(new Sight("Liefdeszuster", new Geopoint(new BasicGeoposition() { Latitude = 51.59307, Longitude = 4.77969 }), Enums.EnumCat.CULTURE));
+            sights.Add(new Sight("Valkenberg", new Geopoint(new BasicGeoposition() { Latitude = 51.59250, Longitude = 4.77969 }), Enums.EnumCat.PARK));
+            sights.Add(new Sight("Nassau Baronie Monument", new Geopoint(new BasicGeoposition() { Latitude = 51.59250, Longitude = 4.77969 }), Enums.EnumCat.CULTURE));
+            sights.Add(new Sight("The Light House", new Geopoint(new BasicGeoposition() { Latitude = 51.59256, Longitude = 4.77889 }), Enums.EnumCat.CULTURE));
+            sights.Add(new Sight("1e bocht Valkenberg", new Geopoint(new BasicGeoposition() { Latitude = 51.59265, Longitude = 4.77844 }), Enums.EnumCat.ROUTEPOINT));
+            sights.Add(new Sight("2e bocht Valkenberg", new Geopoint(new BasicGeoposition() { Latitude = 51.59258, Longitude = 4.77806 }), Enums.EnumCat.ROUTEPOINT));
+            sights.Add(new Sight("Einde park", new Geopoint(new BasicGeoposition() { Latitude = 51.59059, Longitude = 4.77707 }), Enums.EnumCat.ROUTEPOINT));
         }
 #endregion
     }
