2b39c56f28988a705479fdb6863615185bd89502
bart <bgroffen@avans.nl>
2015-01-05 11:08:19 +0100

    awerawr removed

--- Breda Maps/Breda Maps/Controller/Enums/EnumCat.cs
+++ Breda Maps/Breda Maps/Controller/Enums/EnumCat.cs
@@ -16,32 +16,5 @@ namespace Breda_Maps.Controller.Enums
         FACILITY,
         ROUTEPOINT
     }
-
-    public static class ErrorLevelExtensions
-    {
-        public static string ToFriendlyString(this EnumCat me)
-        {
-            switch (me)
-            {
-                case EnumCat.MUSEUM:
-                    return "This is Museum";
-                case EnumCat.BAR:
-                    return "This is a Bar";
-                case EnumCat.CHURCH:
-                    return "This is a church";
-                case EnumCat.CULTURE:
-                    return "This is a culture";
-                case EnumCat.PARK:
-                    return "This is a Park";
-                case EnumCat.FACILITY:
-                    return "This is a Facility";
-                case EnumCat.ROUTEPOINT:
-                    return "This is a Routepoint";
-                default:
-                    return "WTF?";
-            }
-            
-        }
-    }
 }
 
