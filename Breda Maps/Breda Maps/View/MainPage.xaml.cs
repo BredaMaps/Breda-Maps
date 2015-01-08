7c10a67c2e1d9b49c4f11fdb21073536399106f9
kvandenakkerveken <kevvie_jaguar@hotmail.com>
2015-01-08 11:00:15 +0100

    geofencing added

--- Breda Maps/Breda Maps/View/MainPage.xaml.cs
+++ Breda Maps/Breda Maps/View/MainPage.xaml.cs
@@ -22,6 +22,8 @@ using Windows.Services.Maps;
 using Breda_Maps.Model;
 using Windows.UI;
 using System.Threading.Tasks;
+using Windows.Devices.Geolocation.Geofencing;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
@@ -52,6 +54,8 @@ namespace Breda_Maps.View
         {
             this.InitializeComponent();
             _rc.SetMap(this);
+
+            CreateGeofence(StartPosition.Latitude, StartPosition.Longitude, 30);
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -177,6 +181,7 @@ namespace Breda_Maps.View
                 //MapControl1.Center = new Geopoint(CurrentPosition);
             }
             });
+            GeofenceMonitor.Current.GeofenceStateChanged += OnGeofenceStateChanged;
         }
 
         private async Task SmoothSetPosition(Geopoint pos)
@@ -236,5 +241,64 @@ namespace Breda_Maps.View
         //protected override void OnNavigatedTo(NavigationEventArgs e)
         //{
         //}
+
+        private void CreateGeofence(double latitude, double longitude, double radius)
+        {
+            var id = string.Format("Posisition: {0}, {1}", latitude, longitude);
+
+            // Sets the center of the Geofence.
+            var position = new BasicGeoposition
+            {
+                Latitude = latitude,
+                Longitude = longitude
+            };
+
+            // The Geofence is a circular area centered at (latitude, longitude) point, with the
+            // radius in meter.
+            var geocircle = new Geocircle(position, radius);
+
+            // Sets the events that we want to handle: in this case, the entrace and the exit
+            // from an area of intereset.
+            var mask = MonitoredGeofenceStates.Entered | MonitoredGeofenceStates.Exited;
+
+            // Specifies for how much time the user must have entered/exited the area before 
+            // receiving the notification.
+            var dwellTime = TimeSpan.FromSeconds(5);
+
+            // Creates the Geofence and adds it to the GeofenceMonitor.
+            var geofence = new Geofence(id, geocircle, mask, false, dwellTime);
+            GeofenceMonitor.Current.Geofences.Add(geofence);
+        }
+
+        private async void OnGeofenceStateChanged(GeofenceMonitor sender, object e)
+        {
+            var reports = sender.ReadReports();
+
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+            {
+                foreach (var report in reports)
+                {
+                    var state = report.NewState;
+                    var geofence = report.Geofence;
+
+                    if (state == GeofenceState.Entered)
+                    {
+                        // User has entered the area.
+                        ShowMessage("you have entered the geofence");
+                    }
+                    else if (state == GeofenceState.Exited)
+                    {
+                        // User has exited from the area.
+                        ShowMessage("you have exited the geofence");
+                    }
+                }
+            });
+        }
+        async Task ShowMessage(string message)
+        {
+            MessageDialog dialog = new MessageDialog(message);
+            await dialog.ShowAsync();
+        }
+
     }
 }
